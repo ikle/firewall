190833537512d172b141ee5ab121e687422b4fb4
Alexei A. Smekalkine <ikle@ikle.ru>
2020-11-09 20:12:44 +0300

    zone-compile: pass type as parameter into compiler

--- zone-compile-service.c
+++ zone-compile-service.c
@@ -19,8 +19,6 @@ static const char *local_in  = "ZONE_LOCAL_IN";
 static const char *forward   = "ZONE_FORWARD";
 static const char *local_out = "ZONE_LOCAL_OUT";
 
-static const char *type = "firewall";
-
 static int verbose;
 
 static void emit (const char *fmt, ...)
@@ -44,7 +42,7 @@ static int get_zone_chain (const char *zone, char *chain)
 	return snprintf (chain, CHAIN_SIZE, "ZONE-%s-IN", zone) < CHAIN_SIZE;
 }
 
-static int get_policy_chain (const char *policy, char *chain)
+static int get_policy_chain (const char *type, const char *policy, char *chain)
 {
 	return get_chain_hash (type, policy, NULL, chain);
 }
@@ -84,6 +82,7 @@ append_default (struct conf *root, const char *chain, const char *zone,
 
 struct policy_ctx {
 	struct xtc *h;
+	const char *type;
 	const char *zone;
 	const char *chain;
 	struct xt_rule *rule;
@@ -113,16 +112,16 @@ static int in_policy_cb (struct conf *root, char *peer, void *cookie)
 	char policy[CHAIN_SIZE], target[CHAIN_SIZE];
 
 	if (!conf_fetch (root, policy, sizeof (policy),
-			 o->zone, "from", peer, "policy", type, NULL))
+			 o->zone, "from", peer, "policy", o->type, NULL))
 		return 1;
 
-	emit ("D: %s from %s policy %s %s\n", o->zone, peer, type, policy);
+	emit ("D: %s from %s policy %s %s\n", o->zone, peer, o->type, policy);
 
-	if (!get_policy_chain (policy, target))
+	if (!get_policy_chain (o->type, policy, target))
 		return 0;
 
 	if (!xtc_is_chain (o->h, target)) {
-		emit ("E: Policy %s %s does not exists\n", type, policy);
+		emit ("E: Policy %s %s does not exists\n", o->type, policy);
 		errno = ENOENT;
 		return 0;
 	}
@@ -144,16 +143,16 @@ static int out_policy_cb (struct conf *root, char *peer, void *cookie)
 	char policy[CHAIN_SIZE], target[CHAIN_SIZE];
 
 	if (!conf_fetch (root, policy, sizeof (policy),
-			 peer, "from", o->zone, "policy", type, NULL))
+			 peer, "from", o->zone, "policy", o->type, NULL))
 		return 1;
 
-	emit ("D: %s from %s policy %s %s\n", o->zone, peer, type, policy);
+	emit ("D: %s from %s policy %s %s\n", o->zone, peer, o->type, policy);
 
-	if (!get_policy_chain (policy, target))
+	if (!get_policy_chain (o->type, policy, target))
 		return 0;
 
 	if (!xtc_is_chain (o->h, target)) {
-		emit ("E: Policy %s %s does not exists\n", type, policy);
+		emit ("E: Policy %s %s does not exists\n", o->type, policy);
 		errno = ENOENT;
 		return 0;
 	}
@@ -169,68 +168,69 @@ static int out_policy_cb (struct conf *root, char *peer, void *cookie)
 	return 1;
 }
 
-static int
-create_zone_chain (struct conf *root, const char *zone, struct xtc *o)
+static int create_zone_chain (struct conf *root, struct policy_ctx *o)
 {
 	char chain[CHAIN_SIZE];
-	struct policy_ctx p = {o, zone, chain};
 
-	emit ("D: create_zone_chain (%s)\n", zone);
+	o->chain = chain;
 
-	if (!get_zone_chain (zone, chain) || !xtc_create_chain (o, chain))
+	emit ("D: create_zone_chain (%s)\n", o->zone);
+
+	if (!get_zone_chain (o->zone, chain) || !xtc_create_chain (o->h, chain))
 		return 0;
 
-	return	conf_iterate (root, in_policy_cb, &p, zone, "from", NULL) &&
-		append_default (root, chain, zone, o);
+	return	conf_iterate (root, in_policy_cb, o, o->zone, "from", NULL) &&
+		append_default (root, chain, o->zone, o->h);
 }
 
-static int connect_transit (struct conf *root, const char *zone, struct xtc *o)
+static int connect_transit (struct conf *root, struct policy_ctx *o)
 {
 	char target[CHAIN_SIZE];
-	struct policy_ctx p = {o, zone, forward};
 
-	emit ("D: connect_transit (%s)\n", zone);
+	o->chain = forward;
 
-	if (!get_zone_chain (zone, target))
+	emit ("D: connect_transit (%s)\n", o->zone);
+
+	if (!get_zone_chain (o->zone, target))
 		return 0;
 
-	if ((p.rule = xt_rule_alloc (o)) == NULL)
+	if ((o->rule = xt_rule_alloc (o->h)) == NULL)
 		return 0;
 
-	xt_rule_set_goto (p.rule, target);
-	conf_iterate (root, out_rule_cb, &p, zone, "interface", NULL);
+	xt_rule_set_goto (o->rule, target);
+	conf_iterate (root, out_rule_cb, o, o->zone, "interface", NULL);
 
-	xt_rule_free (p.rule);
+	xt_rule_free (o->rule);
 	return 1;
 }
 
-static int connect_local_in (struct conf *root, const char *zone, struct xtc *o)
+static int connect_local_in (struct conf *root, struct policy_ctx *o)
 {
 	char target[CHAIN_SIZE];
 	struct xt_rule *r;
 	int ok;
 
-	emit ("D: connect_local_in (%s)\n", zone);
+	emit ("D: connect_local_in (%s)\n", o->zone);
 
-	if (!get_zone_chain (zone, target) ||
-	    (r = xt_rule_alloc (o)) == NULL)
+	if (!get_zone_chain (o->zone, target) ||
+	    (r = xt_rule_alloc (o->h)) == NULL)
 		return 0;
 
 	xt_rule_set_goto (r, target);
-	ok = xtc_append_rule (o, local_in, r);
+	ok = xtc_append_rule (o->h, local_in, r);
 
 	xt_rule_free (r);
 	return ok;
 }
 
-static int connect_local_out (struct conf *root, const char *zone, struct xtc *o)
+static int connect_local_out (struct conf *root, struct policy_ctx *o)
 {
-	struct policy_ctx p = {o, zone, local_out};
+	o->chain = local_out;
 
-	emit ("D: connect_local_out (%s)\n", zone);
+	emit ("D: connect_local_out (%s)\n", o->zone);
 
-	return	conf_iterate (root, out_policy_cb, &p, zone, "from", NULL) &&
-		append_default (root, local_out, zone, o);
+	return	conf_iterate (root, out_policy_cb, o, o->zone, "from", NULL) &&
+		append_default (root, local_out, o->zone, o->h);
 }
 
 void zone_fini (struct xtc *o)
@@ -256,25 +256,29 @@ void zone_fini (struct xtc *o)
 
 static int zone_chain_cb (struct conf *root, char *zone, void *cookie)
 {
-	struct xtc *o = cookie;
+	struct policy_ctx *o = cookie;
 
-	return create_zone_chain (root, zone, o);
+	o->zone = zone;
+	return create_zone_chain (root, o);
 }
 
 static int zone_policy_cb (struct conf *root, char *zone, void *cookie)
 {
-	struct xtc *o = cookie;
+	struct policy_ctx *o = cookie;
+
+	o->zone = zone;
 
 	return conf_exists (root, zone, "local-zone", NULL) ? (
-		connect_local_in  (root, zone, o) &&
-		connect_local_out (root, zone, o)
+		connect_local_in  (root, o) &&
+		connect_local_out (root, o)
 	) :
-		connect_transit (root, zone, o);
+		connect_transit (root, o);
 }
 
-int zone_init (struct xtc *o)
+int zone_init (struct xtc *o, const char *type)
 {
 	struct conf *root;
+	struct policy_ctx p = {o, type};
 	int ok;
 
 	emit ("D: zone_init ()\n");
@@ -282,8 +286,8 @@ int zone_init (struct xtc *o)
 	if ((root = conf_clone (NULL, "zone-policy", "zone", NULL)) == NULL)
 		return 0;
 
-	ok = conf_iterate (root, zone_chain_cb,  o, NULL) &&
-	     conf_iterate (root, zone_policy_cb, o, NULL) &&
+	ok = conf_iterate (root, zone_chain_cb,  &p, NULL) &&
+	     conf_iterate (root, zone_policy_cb, &p, NULL) &&
 	     xtc_commit (o);
 
 	conf_free (root);
@@ -308,7 +312,7 @@ int main (int argc, char *argv[])
 
 	zone_fini (o);
 
-	if (!zone_init (o)) {
+	if (!zone_init (o, "firewall")) {
 		emit ("E: %s\n", xtc_error (PF_INET));
 		xtc_free (o);
 		return 1;
