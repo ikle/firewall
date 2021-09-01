1a8a1df9098bf228ed809417e4c6723f852bcbc5
Alexei A. Smekalkine <ikle@ikle.ru>
2021-09-01 22:53:10 +0300

    conf: fix typo in copyright

--- conf.h
+++ conf.h
@@ -1,7 +1,7 @@
 /*
  * Configuration Interface
  *
- * Copyright (c) 2018-202 Alexei A. Smekalkine <ikle@ikle.ru>
+ * Copyright (c) 2018-2020 Alexei A. Smekalkine <ikle@ikle.ru>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
